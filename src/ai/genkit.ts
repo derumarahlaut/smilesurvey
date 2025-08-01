3129daf0083825bad385f62894b21a1ad9c03fe6
Deru Marah Laut <derumarahlaut@gmail.com>
2025-08-01 10:00:36 +0000

    Masih seperti ini

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -6,6 +6,8 @@ import { config } from 'dotenv';
 config();
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  })],
   model: 'googleai/gemini-2.0-flash',
 });
