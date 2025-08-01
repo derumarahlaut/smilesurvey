6a5f2e9ea24ccbd6ff67f290567ce55ca6bc52fa
Deru Marah Laut <derumarahlaut@gmail.com>
2025-08-01 02:55:43 +0000

    Untuk tanggal lahir, pilih menu kalendar yang dapat memunculkan pilihan

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -19,11 +19,15 @@ function Calendar({
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
+      captionLayout="dropdown-buttons"
+      fromYear={1924}
+      toYear={new Date().getFullYear()}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-sm font-medium hidden",
+        caption_dropdowns: "flex gap-2",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
